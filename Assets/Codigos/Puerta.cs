dccfbf724254f2c6c8eca8b4bf1cd53580810453
DUVER89 <duveralexanderpradasaboya@gmail.com>
2025-11-28 12:03:39 -0500

    Initial commit

---
+++ Assets/Codigos/Puerta.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Puerta : MonoBehaviour
+{
+    public Animator puertaAnimacion;
+    public AudioSource fuenteAudio;
+    public AudioClip sonidoPuerta;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            puertaAnimacion.Play("Abrir");
+            fuenteAudio.PlayOneShot(sonidoPuerta);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            puertaAnimacion.Play("Cerrar");
+            fuenteAudio.PlayOneShot(sonidoPuerta);
+        }
+    }
+}
